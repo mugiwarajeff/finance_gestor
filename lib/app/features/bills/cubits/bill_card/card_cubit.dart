69727348fad71a31aa61ab38ae4ffb2a73415dc3
Jefferson <jeffersoncerqueira75@gmail.com>
2023-06-19 01:28:13 -0300

    fixing card cubit

--- lib/app/features/bills/cubits/bill_card/card_cubit.dart
+++ lib/app/features/bills/cubits/bill_card/card_cubit.dart
@@ -5,27 +5,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CardCubit extends Cubit<CardState> {
-  final LoadedCardState paidCardState = LoadedCardState(
+  final LoadedCardState _paidCardState = LoadedCardState(
       principalColor: Colors.green,
       secondaryColor: const Color.fromARGB(30, 105, 240, 175),
       billStates: BillStates.paid);
 
-  final LoadedCardState overdueCardState = LoadedCardState(
+  final LoadedCardState _overdueCardState = LoadedCardState(
       principalColor: Colors.orange,
       secondaryColor: const Color.fromARGB(30, 255, 172, 64),
       billStates: BillStates.overdue);
 
-  final LoadedCardState openCardState = LoadedCardState(
+  final LoadedCardState _openCardState = LoadedCardState(
       principalColor: Colors.blue,
       secondaryColor: const Color.fromARGB(30, 68, 137, 255),
       billStates: BillStates.open);
+
+  LoadedCardState get paidCardState => _paidCardState;
+  LoadedCardState get overdueCardState => _overdueCardState;
+  LoadedCardState get openCardState => _openCardState;
+
   CardCubit(Bill bill) : super(InitialCardState()) {
     _getCardState(bill);
   }
 
   void _getCardState(Bill bill) {
     if (bill.paid) {
-      emit(paidCardState);
+      emit(_paidCardState);
     } else {
       _verifyIfOverdue(bill);
     }
@@ -33,9 +38,9 @@ class CardCubit extends Cubit<CardState> {
 
   void _verifyIfOverdue(Bill bill) {
     if (DateTime.now().isAfter(bill.dueDate)) {
-      emit(overdueCardState);
+      emit(_overdueCardState);
     } else {
-      emit(openCardState);
+      emit(_openCardState);
     }
   }
 }
