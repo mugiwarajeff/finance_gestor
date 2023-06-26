747681f997261c9e7caa40746e3c1995be18cb83
Jefferson <jeffersoncerqueira75@gmail.com>
2023-06-26 20:23:18 -0300

    adding crashlytics configuration to project

---
+++ lib/app/main_tasks.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
+
+class MainTasks {
+  static Future<void> initFireBase() async {
+    await Firebase.initializeApp();
+  }
+
+  static void configureCrashlytics() {
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:finance_gestor/app/features/bills/cubits/bills_list/bills_list_bloc.dart';
+import 'package:finance_gestor/app/main_tasks.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'app/app.dart';
 
-void main() => runApp(MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => BillsListCubit(),
-        ),
-      ],
-      child: MyApp(),
-    ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await MainTasks.initFireBase();
+  MainTasks.configureCrashlytics();
+
+  return runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider(
+        create: (context) => BillsListCubit(),
+      ),
+    ],
+    child: MyApp(),
+  ));
+}
